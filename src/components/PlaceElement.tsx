60e2f7fbbf42a4b5d316a0a077c298a943188cbb
TomaszLaboj <tomasz.laboj@gmail.com>
2023-07-03 18:39:21 +0100

    added alt prop

--- src/components/PlaceElement.tsx
+++ src/components/PlaceElement.tsx
@@ -8,7 +8,7 @@ interface PlaceElementProps {
 function PlaceElement(props: PlaceElementProps): JSX.Element {
   return (
     <ul>
-      <img src={props.imagename} />
+      <img src={props.imagename} alt={props.imagename}/>
       <h3>{props.name}</h3>
       <li>{props.location}</li>
       <li>{props.description}</li>
